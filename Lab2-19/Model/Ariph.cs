d8e6cf967d2bf2611f2862b74b58062118acad69
fox_itleksandr <fox_it@bk.ru>
2022-03-17 15:10:52 +0300

    Add project files.

---
+++ Lab2-19/Model/Ariph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_19.Model
+{
+    static class Ariph
+    {
+        public static int Add(int a, int b) => a + b;
+        public static double LenFromRadius(double Radius) => 2 * Math.PI * Radius;
+    }
+}
---
+++ Lab2-19/ViiwModel/MainWindowViewModel.cs
@@ -0,0 +1,118 @@
+﻿using Lab2_19.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab2_19.ViiwModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int number1;
+        private int number2;
+        private int numberSum;
+
+        private double radius;
+        private double lenCricle;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+
+        public int Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int NumberSum
+        {
+            get => numberSum;
+            set
+            {
+                numberSum = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double LenCricle
+        {
+            get => lenCricle;
+            set
+            {
+                lenCricle = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            NumberSum = Ariph.Add(Number1, Number2);
+        }
+
+        private bool CanAddCommandExecute(object p)
+        {
+            if (number1 != 0 || number2 != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public ICommand LenFromRadiusCommand { get; }
+
+        private void OnLenFromRadius(object p)
+        {
+            LenCricle = Ariph.LenFromRadius(Radius);
+        }
+
+        private bool CanLenFromRadius(object p)
+        {
+            if (radius != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+            LenFromRadiusCommand = new RelayCommand(OnLenFromRadius, CanLenFromRadius);
+        }
+    }
+}
+
+
